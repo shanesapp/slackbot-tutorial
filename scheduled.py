71afde9916523eb393dd36a89240ae752b0dbca9
shanesapp <46792604+shanesapp@users.noreply.github.com>
2022-10-04 15:59:19 -0400

    Commit 1

--- scheduled.py
+++ scheduled.py
@@ -13,7 +13,7 @@ def sendMessage(slack_client, msg):
   # check if the request was a success
   try:
     slack_client.chat_postMessage(
-      channel='#test',
+      channel='#bot-test',
       text=msg
     )#.get()
   except SlackApiError as e:
@@ -21,13 +21,13 @@ def sendMessage(slack_client, msg):
     logging.error(e.response)
 
 if __name__ == "__main__":
-  SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
+  SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
   slack_client = WebClient(SLACK_BOT_TOKEN)
   logging.debug("authorized slack client")
 
   # # For testing
   msg = "Good Morning!"
-  schedule.every(60).seconds.do(lambda: sendMessage(slack_client, msg))
+  schedule.every(5).seconds.do(lambda: sendMessage(slack_client, msg))
 
   # schedule.every().monday.at("13:15").do(lambda: sendMessage(slack_client, msg))
   logging.info("entering loop")
